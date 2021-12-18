d691a67371e775866faf7e1d29aabd8093af0f9e
David Minnerly <voxeldavid@gmail.com>
2021-12-18 09:53:13 -0800

    Add test runner

---
+++ tests/spec.lua
@@ -0,0 +1,9 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
+
+TestEZ.TestBootstrap:run({
+	ReplicatedStorage,
+})
+
+return nil
