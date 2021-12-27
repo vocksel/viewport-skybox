d8d9fec1aa1c72865b7727d0eae2003c7bd09fb0
David Minnerly <me@vocksel.com>
2021-12-26 18:06:46 -0800

    Fix installation through Wally

--- tests/spec.lua
+++
@@ -1,9 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
-
-TestEZ.TestBootstrap:run({
-	ReplicatedStorage,
-})
-
-return nil
